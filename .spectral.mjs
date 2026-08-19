60c35bb04fe1e4a12dffd4cd55fa7dd242559391
Aditya Patadia <aditya@gumlet.com>
2026-08-19 15:05:15 +0530

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjMxOTA1Nw/spectral.js?branch_slug=main&token=59a8f42e-c58d-475e-b73c-ca4fbad0a9b4";
+export default { extends: ruleset };
